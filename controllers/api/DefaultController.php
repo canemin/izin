44f5ee126164f984062d4ef4c4e891e4da5d86f0
canemin <43874611+canemin@users.noreply.github.com>
2018-12-27 13:48:18 +0300

    Add files via upload

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\izin\controllers\api;
+
+
+/**
+ * Default controller for the `izin` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\izin\controllers\frontend;
+
+
+/**
+ * Default controller for the `izin` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
